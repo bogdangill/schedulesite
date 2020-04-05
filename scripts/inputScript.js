7bb262b646c52e87fd6cfce6b8b14dcb2793e1a2
bogdangill <herecomesthefog@gmail.com>
2020-04-05 22:51:30 +0500

    made folder for scripts/moved my inputscript there

---
+++ scripts/inputScript.js
@@ -0,0 +1,36 @@
+//этот скрипт предназначен для создания ячейки с текстом, который вводится в textarea. я разработал его для
+//работы своего планнера
+//сначала я использовал изученный на htmlacademy алгоритм по реализации работы кнопки submitbutton
+//потом я решил добавить сценарий-метод на весь документ с горячими клавишами(изучил на learnjavascript)
+//когда у меня это получилось, то я решил упростить код и избавиться от копипасты основного алгоритма функции
+//и изучил this(спасибо статье на хабре). даже конструкторы с классами оказались необязательны(которые я так
+//до конца и не понял).
+//(03/04/20) вонючий карантин, денег нет, никуда не выйти..
+
+let planList = document.querySelector(".plan-list");
+let plannerForm = document.querySelector(".planner-form");
+let buttonSubmit = document.querySelector(".btn-submit");
+let textArea = document.querySelector("textarea");
+
+//сейчас я создаю функцию, которая делает блок с созданием и отправкой комментария/плана из поля для ввода
+//она общая и не выполняется в сценарии скрипта(висит в памяти), пока ее не вызовут через ссылку this
+//так я изучил на практике, что такое этот this и зачем он нужен
+function Submitting() {
+    newElement = document.createElement('li');
+    planList.append(newElement);
+    newElement.classList.add('plan-list-item');
+    newElement.textContent = textArea.value;
+    textArea.value = '';
+};
+
+document.addEventListener('keydown'/*при нажатии*/, function(event) { //добавляю сценарий-ивент на весь документ по нажатию комбинации ctrl + enter (p.s. metaKey - это клавиша windows или cmd у mac)
+    if (event.code == 'Enter' && event.ctrlKey) {
+        //сейчас будет ссылка на ту самую функцию. удобно, что ее можно вызывать хоть где и хоть сколько, не копируя и вставляя ее каждый раз
+        this.submitting = Submitting();
+    }
+});
+
+buttonSubmit.onclick = function(evt) {
+    evt.preventDefault();//это нужно для того, чтобы при нажатии на кнопку, которая располагается в форме, текст в <textarea> не отправлялся куда-то там, а добавлялся в созданный li.
+    this.submitting = Submitting();
+};
