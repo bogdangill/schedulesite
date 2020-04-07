0d3ad974042b3e0988e5b277b38c54ac2e02e2fa
bogdangill <herecomesthefog@gmail.com>
2020-04-07 14:46:46 +0500

    closebutton has been animated

--- scripts/inputScript.js
+++ scripts/inputScript.js
@@ -11,7 +11,6 @@ let planList = document.querySelector(".plan-list");
 let plannerForm = document.querySelector(".planner-form");
 let buttonSubmit = document.querySelector(".btn-submit");
 let textArea = document.querySelector("textarea");
-let 
 
 //сейчас я создаю функцию, которая делает блок с созданием и отправкой комментария/плана из поля для ввода
 //она общая и не выполняется в сценарии скрипта(висит в памяти), пока ее не вызовут через ссылку this
@@ -26,6 +25,10 @@ function Submitting() {
     planListItem.append(closeButton);
     closeButton.classList.add('close-button');
     closeButton.textContent = 'x';
+
+    closeButton.onclick = function() {
+        planList.removeChild(planListItem);
+    }
 };
 
 document.addEventListener('keydown'/*при нажатии*/, function(event) { //добавляю сценарий-ивент на весь документ по нажатию комбинации ctrl + enter (p.s. metaKey - это клавиша windows или cmd у mac)
@@ -39,4 +42,3 @@ buttonSubmit.onclick = function(evt) {
     evt.preventDefault();//это нужно для того, чтобы при нажатии на кнопку, которая располагается в форме, текст в <textarea> не отправлялся куда-то там, а добавлялся в созданный li.
     this.submitting = Submitting();
 };
-
