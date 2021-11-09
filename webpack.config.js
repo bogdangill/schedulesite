0e8080e7b3b4dd29d07411d2a0c3e71db77a9263
bogdangill <herecomesthefog@gmail.com>
2021-11-09 20:37:24 +0500

    add base modules & plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
@@ -9,8 +9,38 @@ module.exports = {
         path: path.resolve(__dirname, './dist'),
         filename: 'app.bundle.js',
     },
+    
+    devServer: {
+        static: './dist',
+        open: true,
+    },
 
     plugins: [
-        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'todo app',
+            template: './src/template.html',
+        })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.svg$/i,
+                type: 'asset/resource'
+            }
+        ]
+    }
 }
