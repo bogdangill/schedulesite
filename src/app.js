30019dbc1386280f76d9461597e339034cb2df13
bogdangill <herecomesthefog@gmail.com>
2021-11-13 21:48:59 +0500

    add basic template, create markup & styles

--- src/app.js
+++ src/app.js
@@ -1,1 +1,9 @@
-alert('test')
+import appName from './icons/app-name.svg';
+import devIcon from './icons/bogdangill.svg';
+import plusIcon from './icons/plus.svg';
+import './template.css';
+// import { default as Logo } from "./components/logo/logo";
+
+// const logo = new Logo('.header');
+
+// logo.createLogo();
